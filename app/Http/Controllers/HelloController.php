916450269af7a2eb91cfcbdd5452173584fd7268
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-08-31 23:18:45 +0900

    modifield HelloCntroller
    ・indexアクションの追加。

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    //
+  public function index() {
+    $data = ['msg' => 'これはBladeを利用したサンプルです。'];
+    return view('hello.index', $data);
+  }
 }
