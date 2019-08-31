004e6efa76af4a22d56717723b077ecff4714537
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-08-31 23:08:58 +0900

    add index.blade.php
    index.blade.phpの作成。

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>Hello/index</title>
+    <style>
+      body {
+        font-size: 16pt;
+        color: #999;
+      }
+      h1 {
+        font-size: 100pt;
+        text-align: right;
+        color: #f6f6f6;
+        margin: -50px 0 -100px 0;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Blade/Index</h1>
+    <p>{{$msg}}</p>
+  </body>
+</html>
