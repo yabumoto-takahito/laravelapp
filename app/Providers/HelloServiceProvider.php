c1237e96627437c66d46bfdb89d8a8edd874b573
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-01 16:46:07 +0900

    Create view_composer Class
    ・index.blade.phpの修正。
    ・HelloComposer.phpを作成し、ビューコンポーザを作成。
    ・HelloComposerを利用できるよう、HelloServiceProviderを修正。

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -5,29 +5,11 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
-class HelloServiceProvider extends ServiceProvider
-{
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+class HelloServiceProvider extends ServiceProvider {
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+    public function boot() {
         View::composer(
-            'hello.index', function($view) {
-                $view->with('view_message', 'composer message!');
-            }
+            'hello.index', 'App\Http\Composers\HelloComposer'
         );
     }
 }
