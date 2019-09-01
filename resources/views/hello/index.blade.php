9df61b3b639e796814700b37bdbb51f5fe872ba9
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-01 13:44:40 +0900

    Create a layout
    ・helloapp.blade.php（ベースレイアウト）の作成
    ・index.blade.php（継承レイアウト）の作成

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,26 +1,17 @@
-<html>
-  <head>
-    <title>Blade/index</title>
-    <style>
-      body {
-        font-size: 16pt;
-        color: #999;
-      }
-      h1 {
-        font-size: 100pt;
-        text-align: right;
-        color: #f6f6f6;
-        margin: -50px 0 -100px 0;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Blade/Index</h1>
-    <p>{{$msg}}</p>
-    <form method="POST" action="/hello">
-      {{ csrf_field() }}
-      <input type="text" name="msg">
-      <input type="submit">
-    </form>
-  </body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+  copyright 2019 yabumoto
+@endsection
