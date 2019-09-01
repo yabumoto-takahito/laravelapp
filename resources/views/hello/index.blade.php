aaa73909608ca01814240c7e56ba304d395f1084
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-01 15:28:09 +0900

    Create view_composer
    ・HelloServiceProviderの作成。
    ・サービスプロバイダーを登録。

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,9 +9,15 @@
 
 @section('content')
   <p>ここが本文のコンテンツです。</p>
-  <p>必要なだけ記述できます。</p>
 
-  @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビューです。'])
+  <p>
+    Controller value<br>
+    'message' = {{$message}}
+  </p>
+  <p>
+    View composer value<br>
+    'view_message' = {{$view_message}}
+  </p>
 
 @endsection
 
