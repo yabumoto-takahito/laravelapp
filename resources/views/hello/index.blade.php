5ba194e1088b60d5d3364c7dafd1e85241afc4b3
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-01 14:09:11 +0900

    Create component
    ・message.blade.phpの作成（コンポーネント）。
    ・index.blade.phpにmessageコンポーネントを組み込み。

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,20 @@
 @section('content')
   <p>ここが本文のコンテンツです。</p>
   <p>必要なだけ記述できます。</p>
+
+  <!-- messaheコンポーネントの組み込み -->
+  @component('components.message')
+    <!-- スロット：{{}} で指定された変数に値を設定 -->
+    @slot('msg_title')
+    CAUTION!
+    @endslot
+
+    @slot('msg_content')
+    これはメッセージの表示です。
+    @endslot
+  @endcomponent
+  <!-- messaheコンポーネントの組み込み -->
+
 @endsection
 
 @section('footer')
